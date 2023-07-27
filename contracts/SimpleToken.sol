559f08edf0add98ce541607be8cd60455912dd1b
ruggedev <ruggedev@gmail.com>
2023-07-28 03:38:12 +0800

    update hh config

--- contracts/SimpleToken.sol
+++ contracts/SimpleToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.5;
+pragma solidity 0.8.21;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol';
--- deploy/001_SimpleToken.ts
+++ deploy/001_SimpleToken.ts
@@ -1,23 +1,20 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts, getChainId } = hre;
-  const { deploy, get, execute, getOrNull } = deployments;
-  const { deployer } = await getNamedAccounts();
+  const { deployments, getNamedAccounts } = hre
+  const { deploy } = deployments
+  const { deployer } = await getNamedAccounts()
 
-  const factory = await deploy("SimpleToken", {
+  const result = await deploy('SimpleToken', {
     from: deployer,
     args: [deployer, 10000],
     log: true,
     skipIfAlreadyDeployed: true,
-  });
+  })
 
-  await hre.run("verify:verify", {
-    address: factory.address,
-    constructorArguments: [deployer],
-  });
-};
+  deployments.log(`Deployed ERC20: ${result.address}, Deployer: ${deployer}, network: ${hre.network.name}`)
+}
 
-export default func;
-func.tags = ["SimpleToken"];
+export default func
+func.tags = ['SimpleToken']
